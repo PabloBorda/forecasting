731f3e5e336cfb27a23b6b1b9774e4a08e91e852
pborda <pborda@d655ea62-35cd-4016-9882-9cce54bdf6a2>
2016-01-30 16:30:47 +0000

    print error on last quote nil

--- Forecasting/Forecaster.rb
+++ Forecasting/Forecaster.rb
@@ -37,38 +37,41 @@ module Forecasting
       if !@company.all_history.nil?
         current_date = Time.now.strftime("%Y-%m-%d")
         last_quote = @company.last_quote
+        if !last_quote.nil?
+          ##puts "LAST QUOTE IS " + last_quote.inspect
 
-        ##puts "LAST QUOTE IS " + last_quote.inspect
+          points_from_line = @company.all_history.similar_points_not_stepping_on_each_other(last_quote,amount_of_days)[0..(amount_of_days*2-1)]
+          ##puts "POINTS_FROM_LINE: " + points_from_line.size.to_s
+          points_with_chunks = []
 
-        points_from_line = @company.all_history.similar_points_not_stepping_on_each_other(last_quote,amount_of_days)[0..(amount_of_days*2-1)]
-        ##puts "POINTS_FROM_LINE: " + points_from_line.size.to_s
-        points_with_chunks = []
-
-        points_from_line.each {|p|
-          if !p.nil?
-            p_index_in_all_history = @company.all_history.find_quote(p.quote)
-            if !p_index_in_all_history.nil?
-              a = []
-              b = []
-              next_chunk = Chunk.new(a)
-              previous_chunk = Chunk.new(b)
-              if (((p_index_in_all_history-amount_of_days-1)>=0) and
-              ((p_index_in_all_history-1)<@company.all_history.size))
-                next_chunk = Chunk.new(@company.all_history.data[(p_index_in_all_history-amount_of_days-1)..(p_index_in_all_history-1)].reverse)
+          points_from_line.each {|p|
+            if !p.nil?
+              p_index_in_all_history = @company.all_history.find_quote(p.quote)
+              if !p_index_in_all_history.nil?
+                a = []
+                b = []
+                next_chunk = Chunk.new(a)
+                previous_chunk = Chunk.new(b)
+                if (((p_index_in_all_history-amount_of_days-1)>=0) and
+                ((p_index_in_all_history-1)<@company.all_history.size))
+                  next_chunk = Chunk.new(@company.all_history.data[(p_index_in_all_history-amount_of_days-1)..(p_index_in_all_history-1)].reverse)
+                end
+                if (((p_index_in_all_history+1)>=0) and
+                ((p_index_in_all_history+amount_of_days+1)<@company.all_history.size))
+                  previous_chunk = Chunk.new(@company.all_history.data[(p_index_in_all_history+1)..(p_index_in_all_history+amount_of_days+1)].reverse)
+                end
+                points_with_chunks.push(Point.new(p.quote,previous_chunk,next_chunk))
               end
-              if (((p_index_in_all_history+1)>=0) and
-              ((p_index_in_all_history+amount_of_days+1)<@company.all_history.size))
-                previous_chunk = Chunk.new(@company.all_history.data[(p_index_in_all_history+1)..(p_index_in_all_history+amount_of_days+1)].reverse)
-              end
-              points_with_chunks.push(Point.new(p.quote,previous_chunk,next_chunk))
             end
-          end
-        }
+          }
 
-        points_with_chunks = points_with_chunks.compact
-        #puts "POINTS_WITH_CHUNKS" + (points_with_chunks.inject("["){|o,p| o = o + p.to_j + ","})[0..-2] + "]"
+          points_with_chunks = points_with_chunks.compact
+          #puts "POINTS_WITH_CHUNKS" + (points_with_chunks.inject("["){|o,p| o = o + p.to_j + ","})[0..-2] + "]"
 
-        points_with_chunks
+          points_with_chunks
+        end
+      else
+        puts "ERROR - Could not retrieve last quote for symbol: " + @company.symbol
       end
     end
 
