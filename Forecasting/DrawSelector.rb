373bfd1f6b8df3c4b7c683d3b24f4f60b12de6c3
pborda <pborda@d655ea62-35cd-4016-9882-9cce54bdf6a2>
2016-01-30 16:19:38 +0000

    draw selector was calling .close to nil quote

--- Forecasting/DrawSelector.rb
+++ Forecasting/DrawSelector.rb
@@ -40,14 +40,17 @@ module Forecasting
   
     def draw_horizontal_line(at_quote)   
       #puts "DRAW HORIZONTAL LINE " + @chunk.data_raw.inspect 
-            
-      similar_quotations = @chunk.data_raw.compact.select { |q| 
-              # #puts "DRAW HORIZONTAL LINE Q" + q.inspect
-              # #puts "DRAW HORIZONTAL LINE at_quote " + at_quote.inspect
-               ((q.close.to_f - at_quote.close.to_f).abs <= 0.3)
+      if ((!@chunk.data_row.nil?) and (@chunk.data_raw.size > 0))      
+        similar_quotations = @chunk.data_raw.compact.select { |q| 
+                # #puts "DRAW HORIZONTAL LINE Q" + q.inspect
+                # #puts "DRAW HORIZONTAL LINE at_quote " + at_quote.inspect
+                 ((q.close.to_f - at_quote.close.to_f).abs <= 0.3)
                 
-               }
-      similar_quotations.uniq
+                 }
+        similar_quotations.uniq
+      else
+        []
+      end
     end
     
     
