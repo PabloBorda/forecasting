263dcffc4652808acf2b37a1be23cef9e50a0dfe
pborda <pborda@d655ea62-35cd-4016-9882-9cce54bdf6a2>
2016-02-05 17:05:00 +0000

    this generates cron file !

--- Forecasting/config/schedule.rb
+++ Forecasting/config/schedule.rb
@@ -18,3 +18,50 @@
 # end
 
 # Learn more: http://github.com/javan/whenever
+
+
+require 'logger'
+
+
+
+@logger = Logger.new('logs/execution.log')
+
+
+job_type :god, 'god -c :configfile'
+job_type :god_load_config, 'god :task :configfile'
+
+
+
+    every 1.day, :at => '9:30 am' do
+      trading_start = {:type => "trading_start"}
+      @logger.info(trading_start) 
+    end
+    
+    every 1.day, :at => '4:00 pm' do
+      trading_finishes = {:type => "trading_finishes"}
+      @logger.info(trading_finishes)
+    end
+     
+      
+    
+    every 1.day, :at => '4:00 pm' do
+      god :configfile => "scripts/accucheck.god"
+    end
+    
+    
+    every 1.day, :at => '4:00 pm' do
+      god :configfile => "scripts/webnews.god"
+    end
+    
+    
+    every 1.day, :at => '4:00 pm' do
+     # @god_lambdas[:word_crawler].call
+    end
+    
+ 
+    every 60.day, :at => '9:30 am' do
+      god_load_config "load", :configfile => "scripts/batchforecasting.god"
+    end
+    
+
+
