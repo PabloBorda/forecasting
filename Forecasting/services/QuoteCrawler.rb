a7ea3de657e8177713edb47d9eb6703f82e31d81
pborda <pborda@d655ea62-35cd-4016-9882-9cce54bdf6a2>
2016-02-25 17:35:03 +0000

    fixes to crawler...now there appear quotes duplicated even using .uniq

--- Forecasting/services/QuoteCrawler.rb
+++ Forecasting/services/QuoteCrawler.rb
@@ -47,14 +47,17 @@ module  Forecasting
           else
             # Get only the missing quotes from the last_update on and append them to the history array and save
             quote_to_update = existing_symbol.to_a[0]
-            b = quote_to_update[:last_update]
-            days_ago = ((Time.now - b)/(24*60*60)).to_i 
+            b = quote_to_update[:history].last[:trade_date]
+            #puts "TRADE DATE" + b.inspect
+            days_ago = ((Time.now - Time.strptime(b.to_s,'%Y-%m-%d'))/(24*60*60)).to_i 
             missing_history = @source.all_history_between(s,{ start_date: Time::now-(24*60*60*days_ago), end_date: Time::now })
+            #puts "MISSING HISTORY " + missing_history.inspect
             if !missing_history.nil?  
-              quote_to_update[:history] = existing_symbol.to_a[0][:history] + missing_history.to_hashes
+              quote_to_update[:history] = (missing_history.to_hashes.uniq + existing_symbol.to_a[0][:history].uniq).uniq.sort {|a,b|  Time.parse(b[:trade_date]) <=> Time.parse(a[:trade_date]) }
               quote_to_update[:last_update] = Time::now
               puts "UPDATING " + s + "..."
               @db[:Quotes].update_one({:symbol => s},quote_to_update)
+              puts "UPDATEd......"
             else
               puts "No missing history for symbol: " + s
             end 
