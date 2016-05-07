48e9cf7ee8cdba13da58e00d849d48d88c241cc6
pborda <pborda@d655ea62-35cd-4016-9882-9cce54bdf6a2>
2016-05-07 07:53:23 +0000

    measure times and insert them to database to detect bottlenecks

--- Forecasting/aspects/TimingAspect.rb
+++ Forecasting/aspects/TimingAspect.rb
@@ -1,4 +1,5 @@
 require 'aspector'
+require_relative '../MongoConnector.rb'
 
 
 
@@ -6,15 +7,18 @@ require 'aspector'
 # Aspect used as a logging hookup
 class TimingAspect < Aspector::Base
   ALL_METHODS = /.*/
-
+  
+  mongo = ::services::MongoConnector.new
+  
   around ALL_METHODS, except: :class, method_arg: true do |method, proxy, *args, &block|
     class_method = "#{self.class}.#{method}"
     puts Time::now.to_s + "| Entering #{class_method}: #{args.join(',')}"
     before_time = Time::now
     result = proxy.call(*args, &block)
     after_time = Time::now - before_time
-    timing_object = {:method_name => class_method, :duration => after_time}
+    timing_object = {:class_name => "#{self.class}",:method_name => class_method, :duration => after_time}
     puts timing_object.to_json
+    mongo[:timing].insert_one(timing_object.to_json) 
     result
   end
 end
