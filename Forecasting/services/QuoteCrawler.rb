77178265a08b73d3177660cf8393b151e13ad7a9
pborda <pborda@d655ea62-35cd-4016-9882-9cce54bdf6a2>
2016-05-14 09:13:59 +0000

    finished

--- Forecasting/services/QuoteCrawler.rb
+++ Forecasting/services/QuoteCrawler.rb
@@ -99,7 +99,7 @@ module  Forecasting
           File.open(Dir.pwd + "/Files/last_symbol_quotecrawler.rb", 'w') {|f| f.write(s+"|"+Date.today.to_s) }
         end
         puts "AMOUNT OF FAILED SYMBOLS: " + failed.to_s + " OF " + @symbols.size.to_s
-
+        puts "FINISHED"
       end
 
       private
